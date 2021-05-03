0a47cc1d0f7974516cd617f9c1082fc74b1e7dba
unknown <Haein>
2021-05-03 16:50:59 +0900

    Change code

--- Concept Study/5. mpg 데이터분석.R
+++ Concept Study/5. mpg 데이터분석.R
@@ -12,7 +12,6 @@ df_mpg <- as.data.frame(ggplot2::mpg) # data.frame 으로 변환
 str(df_mpg)
 
 
-
 # example p.110
 df_raw <- data.frame(var1 = c(1,2,1), var2 = c(2,3,2))
 df_new <- df_raw
@@ -23,10 +22,10 @@ df_new$v_sum <- df_new$v1+df_new$v2
 df_new$v_mean <- (df_new$v1+df_new$v2)/2
 
 
-
 # practice p.112
 df_mpg <- rename(df_mpg, city=cty, highway=hwy)
 df_mpg$total <- (df_mpg$city + df_mpg$highway)/2
+
 #히스토그램 (hist)
 hist(df_mpg$total) 
 
@@ -36,6 +35,7 @@ df_mpg$test <- ifelse(df_mpg$total >= 20, "PASS", "FAIL")
 # facotor형(범주형)이 아니라더도 factor형으로 변환 후 출력
 # 그러나, 모든 함수가 자동적으로 변환 되지 않는다
 table(df_mpg$test)
+
 #막대그래프 (qplot) => test가 pass/fail 범주형데이터이기 때문에 막대그래프 사용!
 qplot(df_mpg$test)
 
