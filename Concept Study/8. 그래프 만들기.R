6d7c246c2058f9512ce3cf41ecfd635b45dd6cf5
unknown <Haein>
2021-05-06 15:41:14 +0900

    Update 0506 study

--- Concept Study/8. 그래프 만들기.R
+++ Concept Study/8. 그래프 만들기.R
@@ -1,10 +1,35 @@
+library(ggplot2)
+library(dplyr)
+mpg <- as.data.frame(ggplot2::mpg)
+
+
+# 막대그래프
+mpg1 <- mpg
+mpg1 <- mpg %>%
+  filter(class=="suv") %>% 
+  group_by(manufacturer) %>% 
+  summarise(mean_cty = mean(cty)) %>% 
+  arrange(desc(mean_cty)) %>% 
+  head(5)
+
+ggplot(data=mpg1, aes(x= reorder(manufacturer,-mean_cty),y=mean_cty)) + geom_col()
+
+
+
+# 박스그래프
+mpg2 <- mpg
+mpg2 <- mpg2 %>% 
+  filter(class %in% c('compact','subcompact','suv'))
+ggplot(data=mpg2, aes(x=class, y=cty))+geom_boxplot()
+
+
+
 # iris 데이터 막대그래프 출력
 install.packages("ggplot2")
 library(ggplot2)
 
 x <- c(3,5,8,11,13)
 y <- c(1,2,3,4,5)
-
 cor(x,y)
 
 # iris 데이터
@@ -20,6 +45,8 @@ plot(iris[,-5])
 cor(iris[,-5])
 
 
+
+# corrplot 패키지
 install.packages("corrplot")
 library(corrplot)
 
@@ -27,6 +54,7 @@ iris_cor <- cor(iris[,-5])
 corrplot(iris_cor, method="circle")
 corrplot(iris_cor, method="ellipse")
 corrplot(iris_cor,method = "shade", tl.col = "blue",tl.srt = 30,diag=FALSE,addCoef.col = "red",order = "AOE")
+
 
 
 # 질적/양적
